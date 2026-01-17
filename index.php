8a417c1bbea93d29cb2d69ecbfdc48adb1967b62
Ahmed Abd ElFattah <ahmedabdelfattah3121@gmail.com>
2026-01-18 01:43:16 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Laravel Application Bootstrap
+ * 
+ * This file serves as a simple bootstrap to load the Laravel application
+ * from the public directory when Document Root cannot be changed.
+ */
+
+// Redirect to public/index.php
+require __DIR__.'/public/index.php';
