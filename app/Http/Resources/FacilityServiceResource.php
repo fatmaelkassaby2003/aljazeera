36fe9b3311f84ba26c68d2bad36a1bd01809e653
Ahmed Abd ElFattah <ahmedabdelfattah3121@gmail.com>
2026-01-22 10:45:24 +0200

    First commit

--- app/Http/Resources/FacilityServiceResource.php
+++ app/Http/Resources/FacilityServiceResource.php
@@ -14,27 +14,33 @@ class FacilityServiceResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $data = parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'description' => $this->description, // مخفي
+            'important_note' => $this->important_note,
+            'icon' => $this->icon,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'types' => $this->types->map(function ($type) {
+                $typeData = [
+                    'id' => $type->id,
+                    'title' => $type->title,
+                    'description' => $type->description, // description الخاص بالـ type
+                    'points' => $type->points,
+                    'items' => $type->items,
+                    'images' => $type->images,
+                ];
 
-        // Fetch types from relationship
-        $data['types'] = $this->types->map(function ($type) {
-            $typeData = [
-                'id' => $type->id,
-                'title' => $type->title,
-                'description' => $type->description,
-                'points' => $type->points,
-                'items' => $type->items, // إضافة البنود هنا
-                'images' => $type->images,
-            ];
-
-            if (isset($typeData['images']) && is_array($typeData['images'])) {
-                $typeData['images'] = array_map(function ($image) {
-                    return asset($image);
-                }, $typeData['images']);
-            }
-            return $typeData;
-        });
-
-        return $data;
+                if (isset($typeData['images']) && is_array($typeData['images'])) {
+                    $typeData['images'] = array_map(function ($image) {
+                        return asset($image);
+                    }, $typeData['images']);
+                }
+                
+                return $typeData;
+            }),
+        ];
     }
 }
